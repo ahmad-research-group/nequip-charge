1264bee21dd2ef828ef7de96cdfd0e1a73aa6795
ahzeeshan <2008.zeeshan@gmail.com>
2025-01-04 18:46:21 -0600

    compatibility change for torch 2 cuda

--- nequip/nn/_ewald.py
+++ nequip/nn/_ewald.py
@@ -115,7 +115,7 @@ class EwaldQeq(GraphModuleMixin, torch.nn.Module):
     def forward(self, data: AtomicDataDict.Type) -> AtomicDataDict.Type:
         device = data[AtomicDataDict.POSITIONS_KEY].device
         species_idx = data[AtomicDataDict.ATOM_TYPE_KEY]
-        sigmas = torch.squeeze(self.sigma[species_idx].to(device), dim =1)
+        sigmas = torch.squeeze(self.sigma.to(device)[species_idx], dim =1)
         chi = self.to_chi(data[AtomicDataDict.NODE_FEATURES_KEY])  # (num_atoms, 1)
         data[AtomicDataDict.ELECTRONEGATIVITY_KEY] = chi
         # square here to restrit hardness to be positive!
